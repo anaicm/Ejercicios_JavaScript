e63e1f6730a58dd82782e24f02b98f77f9846bf9
anaicm <100219176+anaicm@users.noreply.github.com>
2022-11-16 20:22:24 +0100

    Pedir datos y mostrar por pantalla

---
+++ Ejercicio_Tema3/Datos_por_pantalla_JS.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+
+var nombre, apellido, edad;
+nombre=prompt("introduce tú nombre");
+apellido=prompt("introduce tú apellido");
+edad=prompt("introduce tú edad");
+document.write("<p>");
+document.write(nombre +" "+apellido+ " "+edad);
+document.write("</p>");
